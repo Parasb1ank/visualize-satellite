ee3044e88550019651aeed28c9e4badc06e1cd83
Parasb1ank <paras.b1ank@gmail.com>
2022-08-25 19:16:02 +0530

    2

--- cesium/views.py
+++ cesium/views.py
@@ -22,16 +22,15 @@ def satellites(request):
     return render(request, 'sat.html', { "sats" :sats  })
 
 def visualize(request,sat_id):
-    
+
     data = requests.api.get(f"https://tle-backend.herokuapp.com/tlebyid/{sat_id}/").json()
     sat_rec = Satellite(id=f"{sat_id}",name=data["name"],line1=data["line1"],line2=data["line2"])
     sat_rec.save()
 
-    sat = Satellite.objects.get(id=f"{sat_id}")
-    single_tle_list = [[ sat.name,sat.line1,sat.line2, ]]
+    single_tle_list = [[ data["name"],data["line1"],data["line2"], ]]
 
     czml_string = satellite_czml(tle_list=single_tle_list).get_czml()
     open("cesium/static/data.czml",'w').write(czml_string)
 
-    return render(request, 'visualize.html',{ "sat_id": sat_id} )
+    return render(request, 'visualize.html',{ "sat_id": sat_id } )
 
