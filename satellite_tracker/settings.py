e04a9b3ba25d541a33ca5e7da6b03bda807e4e48
Parasb1ank <paras.b1ank@gmail.com>
2022-08-25 08:43:59 +0530

    2

--- satellite_tracker/settings.py
+++ satellite_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2(5^w@$fum^)vr*n^e!2te7s_jd+ntfp*(8=7_wc+9#!i^)4v^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
